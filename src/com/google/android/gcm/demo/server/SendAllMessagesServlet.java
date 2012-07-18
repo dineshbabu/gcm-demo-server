3eaf8f5886fd2b10cc1b69c3bfdc8730bc33919f
Dinesh Babu <dbabu@serena.com>
2012-07-19 00:02:45 +0100

    Hackathone complete

--- src/com/google/android/gcm/demo/server/SendAllMessagesServlet.java
+++ src/com/google/android/gcm/demo/server/SendAllMessagesServlet.java
@@ -20,14 +20,21 @@ import com.google.android.gcm.server.Message;
 import com.google.android.gcm.server.MulticastResult;
 import com.google.android.gcm.server.Result;
 import com.google.android.gcm.server.Sender;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.List;
+
+import javapns.Push;
+import javapns.communication.exceptions.CommunicationException;
+import javapns.communication.exceptions.KeystoreException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -59,13 +66,19 @@ public class SendAllMessagesServlet extends BaseServlet {
     return new Sender(key);
   }
 
-  /**
-   * Processes the request to add a new message.
-   */
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException, ServletException {
-	  if( null != req.getParameter("command") && req.getParameter("command").equals("put") ){//send story details back to device. Temporary until Floz's Servlet is ready
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		if( null != req.getParameter("command") && req.getParameter("command").equals("get") ){//send story details back to device. Temporary until Floz's Servlet is ready
+			  
+		    //resp.setContentType("text/html");
+		    resp.setContentType("application/json");
+		    
+		    PrintWriter out = resp.getWriter();
+		    
+		    out.print("{ \"blocked\": true, \"criteria\": \"This is the insufficnet criteria\", \"id\": \"story id\",   \"owner\": \"dinesh\", \"phase\": \"story phase\", \"title\": \"story title\", \"uid\": \"1\" }");
+
+		    resp.setStatus(HttpServletResponse.SC_OK);
+	  }else if( null != req.getParameter("command") && req.getParameter("command").equals("put") ){//send story details back to device. Temporary until Floz's Servlet is ready
 		  
 		    //resp.setContentType("text/html");
 		    resp.setContentType("application/json");
@@ -75,83 +88,115 @@ public class SendAllMessagesServlet extends BaseServlet {
 		    out.print("{ \"success\": true }");
 
 		    resp.setStatus(HttpServletResponse.SC_OK);
-	  }else if( null != req.getParameter("command") && req.getParameter("command").equals("get") ){//send story details back to device. Temporary until Floz's Servlet is ready
-		  
-		    //resp.setContentType("text/html");
-		    resp.setContentType("application/json");
-		    
-		    PrintWriter out = resp.getWriter();
-		    
-		    out.print("{ \"blocked\": true, \"criteria\": \"This is the insufficnet criteria\", \"id\": \"story id\",   \"owner\": \"dinesh\", \"phase\": \"story phase\", \"title\": \"story title\", \"uid\": \"1\" }");
-
-		    resp.setStatus(HttpServletResponse.SC_OK);
-	  }else{
-		  List<String> devices = Datastore.getDevices();
-		    StringBuilder status = new StringBuilder();
-		    if (devices.isEmpty()) {
-		      status.append("Message ignored as there is no device registered!");
-		    } else {
-		      List<Result> results;
-		      // NOTE: check below is for demonstration purposes; a real application
-		      // could always send a multicast, even for just one recipient
-		      if (devices.size() == 1) {
-		        // send a single message using plain post
-		        String registrationId = devices.get(0);
-		        //Message message = new Message.Builder().build();
-		        Message message = new Message.Builder().addData("blocked", "true")
-		        		.addData("message", "Story blocked notification")
-		        		.addData("uid", "1002")
-		        		.build();        
-		        
-		        Result result = sender.send(message, registrationId, 5);
-		        results = Arrays.asList(result);
-		      } else {
-		        // send a multicast message using JSON
-		        Message message = new Message.Builder().build();
-		        MulticastResult result = sender.send(message, devices, 5);
-		        results = result.getResults();
-		      }
-		      // analyze the results
-		      for (int i = 0; i < devices.size(); i++) {
-		        Result result = results.get(i);
-		        if (result.getMessageId() != null) {
-		          status.append("Succesfully sent message to device #").append(i);
-		          String canonicalRegId = result.getCanonicalRegistrationId();
-		          if (canonicalRegId != null) {
-		            // same device has more than on registration id: update it
-		            logger.finest("canonicalRegId " + canonicalRegId);
-		            devices.set(i, canonicalRegId);
-		            status.append(". Also updated registration id!");
-		          }
-		        } else {
-		          String error = result.getErrorCodeName();
-		          if (error.equals(Constants.ERROR_NOT_REGISTERED)) {
-		            // application has been removed from device - unregister it
-		            status.append("Unregistered device #").append(i);
-		            String regId = devices.get(i);
-		            Datastore.unregister(regId);
-		          } else {
-		            status.append("Error sending message to device #").append(i)
-		                .append(": ").append(error);
-		          }
-		        }
-		        status.append("<br/>");
-		      }
-		    }
-		    req.setAttribute(HomeServlet.ATTRIBUTE_STATUS, status.toString());
-		    getServletContext().getRequestDispatcher("/home").forward(req, resp);
 	  }
+	}
+  /**
+   * Processes the request to add a new message.
+   */
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
+	  		String notificationMessage = "Story blocked notification";
+	  		String android = req.getParameter("android");
+	  		if(null != android && android.equals("false")){//iPhone
+	  			sendiPhoneNotification(notificationMessage);
+	  		}else{
+		  		List<String> devices = Datastore.getDevices();
+			    StringBuilder status = new StringBuilder();
+			    if (devices.isEmpty()) {
+			      status.append("Message ignored as there is no device registered!");
+			    } else {
+			      List<Result> results;
+			      //Read blocked story from file on disk
+			      //String blockedStory = readBlockedStoryFromDisk();
+			      String blockedStory = URLDecoder.decode(req.getParameter("blockedStory"));
+			      Gson gson = new Gson();
+			      StoryDetails storyDetails = gson.fromJson(blockedStory, StoryDetails.class);
+			      
+			      // NOTE: check below is for demonstration purposes; a real application
+			      // could always send a multicast, even for just one recipient
+			      if (devices.size() == 1) {
+			        // send a single message using plain post
+			        String registrationId = devices.get(0);
+			        //Message message = new Message.Builder().build();
+			        /*Message message = new Message.Builder().addData("blocked", "true")
+			        		.addData("message", "Story blocked notification")
+			        		.addData("uid", "1002")
+			        		.build(); */       
+			        
+			        Message message = new Message.Builder().addData("blocked", Boolean.toString(storyDetails.getBlocked()))
+			        		.addData("message", notificationMessage)
+			        		.addData("uid", Long.toString(storyDetails.getUid()))
+			        		.build();
+			        
+			        Result result = sender.send(message, registrationId, 5);
+			        results = Arrays.asList(result);
+			      } else {
+			        // send a multicast message using JSON
+			        Message message = new Message.Builder().build();
+			        MulticastResult result = sender.send(message, devices, 5);
+			        results = result.getResults();
+			      }
+			      // analyze the results
+			      for (int i = 0; i < devices.size(); i++) {
+			        Result result = results.get(i);
+			        if (result.getMessageId() != null) {
+			          status.append("Succesfully sent message to device #").append(i);
+			          String canonicalRegId = result.getCanonicalRegistrationId();
+			          if (canonicalRegId != null) {
+			            // same device has more than on registration id: update it
+			            logger.finest("canonicalRegId " + canonicalRegId);
+			            devices.set(i, canonicalRegId);
+			            status.append(". Also updated registration id!");
+			          }
+			        } else {
+			          String error = result.getErrorCodeName();
+			          if (error.equals(Constants.ERROR_NOT_REGISTERED)) {
+			            // application has been removed from device - unregister it
+			            status.append("Unregistered device #").append(i);
+			            String regId = devices.get(i);
+			            Datastore.unregister(regId);
+			          } else {
+			            status.append("Error sending message to device #").append(i)
+			                .append(": ").append(error);
+			          }
+			        }
+			        status.append("<br/>");
+			      }
+			      req.setAttribute(HomeServlet.ATTRIBUTE_STATUS, status.toString());
+				  getServletContext().getRequestDispatcher("/home").forward(req, resp);
+			    }
+		    
+	  		}
+		    
+	  //}
     
   }
   
-  public void readStoriesFromDisk() throws IOException{
-	  File storyFile = new File("C:\\apache-tomcat-6.0.33\\StoryList.txt");
+  public static void sendiPhoneNotification(String message){
+			try {
+				Push.alert(message, "C:\\iPhoneCert\\SerenaAgilePushDemo.p12", "thisisapassword", false, "39bd946c5c211778819a3acc3dc7a52bab23b91cd9f1882fea5afbc69e590197");
+			} catch (CommunicationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (KeystoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+		
+  }
+  
+  public String readBlockedStoryFromDisk() throws IOException{
+	  File storyFile = new File("C:\\agilator\\delegate.txt");
 	  BufferedReader br = new BufferedReader(new FileReader(storyFile));
 	  String line = "";
-	   
-	  while( (line = br.readLine()) != null ){
+	  return  br.readLine();
+	  /*while( (line = br.readLine()) != null ){
 		  String[] storyDetails = line.split(",");  
-	  }
+	  }*/
   }
 
 }
--- src/com/google/android/gcm/demo/server/Test.java
+++ src/com/google/android/gcm/demo/server/Test.java
@@ -4,24 +4,151 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class Test {
 
+	static long mostRecentDelegate = 0;
+	static long mostRecentBlocked = 0;
+	public static final String DATA_SERVER = "http://172.20.10.5:8080/gcm-demo/sendAll";
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-	  File storyFile = new File("C:\\apache-tomcat-6.0.33\\StoryList.txt");
+	  /*File storyFile = new File("C:\\apache-tomcat-6.0.33\\StoryList.txt");
 	  BufferedReader br = new BufferedReader(new FileReader(storyFile));
 	  String line = "";
-	   
+	   System.out.println(Boolean.valueOf("true"));
 	  while( (line = br.readLine()) != null ){
 		  String[] storyDetails = line.split(",");  
 		  System.out.println(storyDetails);
-	  }
-		
+	  }*/
+		String folderName = "c:\\agilator";
+		String storyFile = "";
+		File file = null;
+		while(true){		
+			
+			file = new File(folderName);
+			
+			//Read delegated stories
+			
+			String delegatedStory = "";
+			
+			String[] delegatedList = file.list(new FilenameFilter(){			
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.endsWith("delegate.json");
+					//return name.endsWith("delegate.json");
+				}
+			});
+			
+			for (int i = 0; i < delegatedList.length; i++) {
+				storyFile = folderName+ "\\"+delegatedList[i];
+				delegatedStory = readStoryFile(storyFile);
+				if(isNewFile(storyFile, false)){
+					sendNotification(DATA_SERVER, delegatedStory, true);
+				}			
+			}
+			
+			//Read blocked stories
+			String blockedStory = "";
+			String[] blockedList = file.list(new FilenameFilter(){			
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.endsWith("block.json");
+					//return name.endsWith("delegate.json");
+				}
+			});
+			
+			for (int i = 0; i < blockedList.length; i++) {
+				storyFile = folderName+ "\\"+blockedList[i];
+				blockedStory = readStoryFile(storyFile);
+				if(isNewFile(storyFile, false)){
+					sendNotification(DATA_SERVER, blockedStory, false);	
+				}			
+			}
+		}
+				
 	}
-
+	
+	public static String readStoryFile(String storyFile) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(storyFile));
+		return br.readLine();  
+	}
+	
+	public static boolean isNewFile(String filePath, boolean blocked){
+		File file = new File(filePath);
+		boolean isNew = false;
+		if(blocked){
+			if( mostRecentBlocked < file.lastModified() ){
+				isNew = true;
+				mostRecentDelegate = file.lastModified();
+			}	
+		}else{
+			if( mostRecentDelegate < file.lastModified() ){
+				isNew = true;
+				mostRecentDelegate = file.lastModified();
+			}
+		}
+		
+		
+		return isNew;
+	}
+	
+	public static String sendNotification(String endpoint, String blockedStory, boolean android)
+            throws IOException {
+        URL url;
+        BufferedReader reader = null;
+        StringBuffer resp = new StringBuffer();
+        String queryString ="blockedStory="+blockedStory+"&android="+android;
+        try {
+            url = new URL(endpoint);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("invalid url: " + endpoint);
+        }
+        HttpURLConnection conn = null;
+        try {
+        	
+        	conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setFixedLengthStreamingMode(queryString.getBytes().length);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded;charset=UTF-8");
+            //conn.setRequestProperty("blockedStory", blockedStory);
+            // post the request
+            OutputStream out = conn.getOutputStream();
+            out.write(queryString.getBytes());
+            out.close();
+        	
+            /*conn = (HttpURLConnection) url.openConnection();
+            conn.setUseCaches(false);
+            conn.setRequestMethod("GET");*/
+            InputStream response = conn.getInputStream();
+            try {
+                reader = new BufferedReader(new InputStreamReader(response));
+                for (String line; (line = reader.readLine()) != null;) {
+                	resp.append(line);
+                }
+            } finally {
+                if (reader != null) try { reader.close(); } catch (IOException logOrIgnore) {}
+            }
+            return resp.toString();
+           
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+      }
 }
